6b176d4ae72601d2909e7a3b36b47828a132d9d4
seancustodio <56618290+seancustodio@users.noreply.github.com>
2019-10-16 00:51:09 -0700

    Updated __main__.py
    Updated main routine to subscribe to 'created' actions for 'repository' events.  Added co-routine 'Repository Event' to grab new repository names and post master branch protections.

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -1,19 +1,60 @@
-import os
+import aiohttp
 
 from aiohttp import web
 
-routes = web.RouteTableDef()
+from gidgethub import routing, sansio
+from gidgethub import aiohttp as gh_aiohttp
 
-@routes.get("/")
+router = routing.Router()
+
+@router.register("repository", action="created")
+async def RepositoryEvent(event, gh, *args, **kwargs):
+    """ Whenever a repository is created, automate the protection of the master branch"""
+
+    # get new repository name
+    newrepo = event.data["repository"]["name"]
+    endpoint = f'/repos/seancustodio/{newrepo}/branches/master/protection'
+    #add master branch protections
+    await gh.post(endpoint,
+              data={
+              	  # required status checks to pass before merging
+				  "required_status_checks": null,
+				  # enforce protections for administrators
+				  "enforce_admins": true,
+				  # require one approving review for pull request
+				  "required_pull_request_reviews": {
+				  	# specify which users can dismiss pull requests
+				    "dismissal_restrictions": null,
+				    # dismiss approving reviews when someone pushes new commit
+				    "dismiss_stale_reviews": false,
+				    # pull requests held until code owner approves
+				    "require_code_owner_reviews": true,
+				    # one reviewer required to approve pull request
+				    "required_approving_review_count": 1
+				  },
+				  # restrict who can push to branch
+				  "restrictions": null
+				})
+
+@routes.post("/")
 async def main(request):
-    return web.Response(status=200, text="Hello world!")
+    # read the GitHub webhook payload
+    body = await request.read()
 
+    # authentication token and secret
+    secret = os.environ.get("GH_SECRET")
+    oauth_token = os.environ.get("GH_AUTH")
 
-if __name__ == "__main__":
-    app = web.Application()
-    app.add_routes(routes)
-    port = os.environ.get("PORT")
-    if port is not None:
-        port = int(port)
+    # a representation of GitHub webhook event
+    event = sansio.Event.from_http(request.headers, body, secret=secret)
 
-    web.run_app(app, port=port)
+    # add username
+    async with aiohttp.ClientSession() as session:
+        gh = gh_aiohttp.GitHubAPI(session, "seancustodio",
+                                  oauth_token=oauth_token)
+
+        # call the appropriate callback for the event
+        await router.dispatch(event, gh)
+
+    # return a "Success"
+    return web.Response(status=200)
